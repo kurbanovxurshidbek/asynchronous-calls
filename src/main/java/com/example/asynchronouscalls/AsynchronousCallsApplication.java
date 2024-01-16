54d6a78ecef5b31001d29eb7554f314243f4856e
khurshid <shamsun.com@gmail.com>
2024-01-16 09:16:38 +0500

    Initial commit

---
+++ src/main/java/com/example/asynchronouscalls/AsynchronousCallsApplication.java
@@ -0,0 +1,17 @@
+package com.example.asynchronouscalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://www.callicoder.com/java-8-completablefuture-tutorial/
+//https://medium.com/globant/asynchronous-calls-in-spring-boot-using-async-annotation-d34d8a82a60c
+//https://www.linkedin.com/pulse/asynchronous-calls-spring-boot-using-async-annotation-omar-ismail/
+
+@SpringBootApplication
+public class AsynchronousCallsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsynchronousCallsApplication.class, args);
+    }
+
+}
